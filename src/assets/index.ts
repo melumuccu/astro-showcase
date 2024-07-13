983773565cfaf533f4aebb9bc404de32c64fa153
koki fujisawa <melumuccu413@gmail.com>
2024-07-13 13:28:54 +0900

    feat: Header > HeaderSampleCafe

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 export { default as Image1 } from './image/1.png';
 export { default as Image2 } from './image/2.png';
 export { default as Image3 } from './image/3.png';
+export { default as LogoCafe } from './logo/cafe.png';
 export { default as LogoGithub } from './logo/github.png';
 export { default as LogoInstagram } from './logo/instagram.png';
 export { default as LogoX } from './logo/x.png';
